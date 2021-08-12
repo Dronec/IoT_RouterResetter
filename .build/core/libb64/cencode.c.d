9dfa010afd4d63848442fc78661df010cfc21e5c
Andrey Baranov <andrey.baranov@quantium.com.au>
2021-08-13 09:25:18 +1000

    first commit

---
+++ .build/core/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+c:\Users\Pups\source\repos\projects\IoTSecurityCamera\Device\.build\core\libb64\cencode.c.o: \
+ C:\Users\Pups\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cencode.c \
+ C:\Users\Pups\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cencode.h
