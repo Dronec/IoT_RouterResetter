22236b73940f68ca90277aac12f5ca40d73cf29d
Andrey Baranov <Andrey.Baranov@3plearning.com>
2022-01-05 21:38:41 +1100

    Fully working version 0.1

--- Device/device.ino
+++ Device/device.ino
@@ -4,15 +4,21 @@
 #include <ESPAsyncWebServer.h>
 #include "LittleFS.h"
 #include <AsyncElegantOTA.h>
+#include <ESP8266HTTPClient.h>
+#include <DefsWiFi.h>
 
-#define NoRebootTime 600000  // 10 min no reboot
+#define NoResetTime 600000   // 10 min no reset
 #define DefaultOffTime 10000 // 10 sec off time
 
-const char *ssid = "ssid";
-const char *password = "password";
+#define ToleranceLimit 120000 // 2 min until reboot
+
+const char *ssid = WIFISSID_1;
+const char *password = WIFIPASS_1;
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
+
+WiFiEventHandler e1;
 
 // Hex command to send to serial for close relay
 // Hex command to send to serial for close relay
@@ -33,6 +39,30 @@ bool pp2Enabled = true;
 unsigned long timer = 0;
 unsigned long pp1offtime = 0;
 unsigned long pp2offtime = 0;
+unsigned long execCount = 0;
+unsigned long lastExternalPingErrorTime = 0;
+
+HTTPClient http;
+WiFiClient client;
+
+void onSTAGotIP(WiFiEventStationModeGotIP ipInfo)
+{
+  Serial.printf("Connected: %s\n", WiFi.localIP().toString().c_str());
+  initWebSocket();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(LittleFS, "/index.html", "text/html", false); });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start ElegantOTA
+  AsyncElegantOTA.begin(&server);
+
+  // Start server
+  server.begin();
+}
+
 // Initialize LittleFS
 void initLittleFS()
 {
@@ -118,13 +148,13 @@ void notifyClients()
 {
   String state;
   if (pp1Enabled)
-    state+="0";
+    state += "0";
   else
-    state+="1";
+    state += "1";
   if (pp2Enabled)
-    state+="0";
+    state += "0";
   else
-    state+="1";
+    state += "1";
   ws.textAll(state);
 }
 
@@ -133,23 +163,42 @@ void setup()
   delay(10);
   Serial.begin(115200);
 
+  Serial.printf("Connecting to: %s\n",ssid);
+
   WiFi.begin(ssid, password);
 
+  e1 = WiFi.onStationModeGotIP(onSTAGotIP);
+
   initLittleFS();
-  initWebSocket();
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/index.html", "text/html", false); });
-
-  server.serveStatic("/", LittleFS, "/");
-
-  // Start ElegantOTA
-  AsyncElegantOTA.begin(&server);
-
-  // Start server
-  server.begin();
   timer = millis();
+}
+
+bool CheckInternet()
+{
+
+  http.begin(client, "http://www.msftncsi.com/ncsi.txt");
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0)
+  {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK)
+    {
+      Serial.println(http.getString());
+      return true;
+    }
+  }
+  else
+  {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    return false;
+  }
+  http.end();
 }
 
 void loop()
@@ -162,62 +211,33 @@ void loop()
   {
     switchRelay(2, true);
   }
-  //if (timer + NoRebootTime < millis())
-  //{
-  //}
-  // int val;
-  /*
-    // Check if a client has connected
-    WiFiClient client = server.available();
-    if (!client)
+  if (timer + NoResetTime < millis())
+  {
+    if (lastExternalPingErrorTime > 0 && lastExternalPingErrorTime + ToleranceLimit < millis())
     {
-      return;
+      switchRelay(1, false);
+      Serial.println("Resetting Power Point #1");
+      lastExternalPingErrorTime = 0;
+      timer = 0;
     }
+    ws.cleanupClients();
+    notifyClients();
 
-    // Wait until the client sends some data
-    while (!client.available())
+    if (execCount % 60 == 0)
     {
-      delay(100);
-    }
+      if (CheckInternet() && lastExternalPingErrorTime > 0)
+      {
+        lastExternalPingErrorTime = 0;
+        Serial.println("Error count reset.");
+      }
 
-    // Read the first line of the request
-    String req = client.readStringUntil('\r');
-    client.flush();
-
-    // Match the request
-    if (req.indexOf("/1/on") != -1)
-    {
-      Serial.write(relON, sizeof(relON));
-      val = 1; // if you want feedback see below
+      if (!CheckInternet() && lastExternalPingErrorTime == 0)
+      {
+        Serial.println("Starting error count...");
+        lastExternalPingErrorTime = millis();
+      }
     }
-    else if (req.indexOf("/1/off") != -1)
-    {
-      Serial.write(relOFF, sizeof(relOFF));
-      val = 0; // if you want feedback
-    }
-    else if (req.indexOf("/2/on") != -1)
-    {
-      Serial.write(rel2ON, sizeof(rel2ON));
-      val = 1; // if you want feedback see below
-    }
-    else
-    {
-      if (req.indexOf("/2/off") != -1)
-        Serial.write(rel2OFF, sizeof(rel2OFF));
-      val = 0; // if you want feedback
-    }
-
-    client.flush();
-
-    // only if you want feedback - see above
-    // Prepare the response
-    String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nRelay is now ";
-    s += (val) ? "on" : "off";
-    s += "</html>\n";
-
-    // Send the response to the client
-    client.print(s);*/
-  ws.cleanupClients();
-  notifyClients();
+  }
   delay(1000);
+  execCount++;
 }
